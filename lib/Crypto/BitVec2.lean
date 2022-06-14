9c534ba06da3c41279b825449c337b684573f784
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-06-14 17:31:54 -0400

    chore: adjust style

--- lib/Crypto/BitVec2.lean
+++ lib/Crypto/BitVec2.lean
@@ -13,7 +13,7 @@ def BitVec (w : Nat) := Fin (2^w)
 @[ext]
 structure BitVec.Packed where
   width : Nat
-  data : Fin (2^width)
+  data  : Fin (2^width)
 
 namespace BitVec
 namespace Packed
@@ -57,10 +57,10 @@ instance : Xor (BitVec w) := ⟨BitVec.xor⟩
 instance : HShiftLeft (BitVec w) Nat (BitVec w) := ⟨BitVec.shiftLeft⟩
 instance : HShiftRight (BitVec w) Nat (BitVec w) := ⟨BitVec.shiftRight⟩
 
-def lsb_get! {m:Nat} (x:BitVec m) (i:Nat) : Bool :=
+def lsb_get! (x : BitVec m) (i : Nat) : Bool :=
   (x.val &&& (1 <<< i)) ≠ 0
 
-def lsb_set! {m:Nat} (x:BitVec m) (i:Nat) (c:Bool) : BitVec m :=
+def lsb_set! (x : BitVec m) (i : Nat) (c : Bool) : BitVec m :=
   if c then
     x ||| ⟨1 <<< i, sorry⟩
   else
@@ -72,25 +72,25 @@ ordering within bytes.
 
 This may be removed once compatibility with C is not needed.
 -/
-def msbb_fix (m:Nat) (i:Nat) : Nat :=
+def msbb_fix (m : Nat) (i : Nat) : Nat :=
   let j := (m-1)-i
   -- Reverse bit order within bytes (see if we can fix this)
   ((j >>> 3) <<< 3) ||| (0x7 - (j &&& 0x7))
 
-def msbb_get! {m:Nat} (x:BitVec m) (i:Nat) : Bool := x.lsb_get! (msbb_fix m i)
+def msbb_get! (x : BitVec m) (i : Nat) : Bool := x.lsb_get! (msbb_fix m i)
 
-def msbb_set! {m:Nat} (x:BitVec m) (i:Nat) (c:Bool) : BitVec m :=
+def msbb_set! (x : BitVec m) (i : Nat) (c : Bool) : BitVec m :=
   x.lsb_set! (msbb_fix m i) c
 
-protected def toBinary (x:BitVec n) : String :=
+protected def toBinary (x : BitVec n) : String :=
   let l := Nat.toDigits 2 x.val
   String.mk (List.replicate (n - l.length) '0' ++ l)
 
-protected def toHex (x:BitVec n) : String :=
+protected def toHex (x : BitVec n) : String :=
   let l := Nat.toDigits 16 x.val
   String.mk (List.replicate (n/4 - l.length) '0' ++ l)
 
-protected def toHex2 (x:BitVec n) : String := Id.run do
+protected def toHex2 (x : BitVec n) : String := Id.run do
   let mut s : String := ""
   for i in range 0 (n/8) do
     let b := UInt8.ofNat (x.val >>> (8*i))
@@ -98,7 +98,7 @@ protected def toHex2 (x:BitVec n) : String := Id.run do
   pure s
 instance : ToString (BitVec n) := ⟨BitVec.toHex2⟩
 
-def reverse (x:BitVec n) : BitVec n := Id.run do
+def reverse (x : BitVec n) : BitVec n := Id.run do
   let mut r : Nat := 0
   for i in range 0 n do
     r := r <<< 1
@@ -106,33 +106,33 @@ def reverse (x:BitVec n) : BitVec n := Id.run do
       r := r + 1
   pure ⟨r, sorry⟩
 
-protected def foldl (f: α → Bool → α) (x: BitVec n) (a : α) : α := Id.run do
+protected def foldl (f : α → Bool → α) (x : BitVec n) (a : α) : α := Id.run do
   let mut r := a
   for i in range 0 n do
     r := f r (x.msbb_get! i)
   pure r
 
-protected def take_lsb (x:BitVec m) (n:Nat) : BitVec n :=
+protected def take_lsb (x : BitVec m) (n : Nat) : BitVec n :=
   ⟨x.val &&& 1 <<< n - 1, sorry⟩
 
-protected def take_msb (x:BitVec m) (n:Nat) : BitVec n :=
+protected def take_msb (x : BitVec m) (n : Nat) : BitVec n :=
   ⟨x.val >>> (m-n), sorry⟩
 
-theorem eq_of_val_eq {n:Nat} : ∀ {x y : BitVec n}, Eq x.val y.val → Eq x y
-  | ⟨x,_⟩, _, rfl => rfl
+theorem eq_of_val_eq : ∀ {x y : BitVec n}, x.val = y.val → x = y
+  | ⟨_,_⟩, _, rfl => rfl
 
-theorem ne_of_val_ne {n:Nat}  {i j : BitVec n} (h : Not (Eq i.val j.val)) : Not (Eq i j) :=
-  λh' => absurd (h' ▸ rfl) h
+theorem ne_of_val_ne {x y : BitVec n} (h : x.val ≠ y.val) : x ≠ y :=
+  λ h' => absurd (h' ▸ rfl) h
 
-protected def decideEq {n:Nat} : (a b : BitVec n) → Decidable (Eq a b) :=
+protected def decideEq : (x y : BitVec n) → Decidable (x = y) :=
   fun ⟨i, _⟩ ⟨j, _⟩ =>
     match decEq i j with
     | isTrue h  => isTrue (eq_of_val_eq h)
     | isFalse h => isFalse (ne_of_val_ne h)
 
-instance (n:Nat) : DecidableEq (BitVec n) := BitVec.decideEq
+instance : DecidableEq (BitVec n) := BitVec.decideEq
 
-protected def generate_lsb (n:Nat) (f : Fin n → Bool) : BitVec n := Id.run do
+protected def generate_lsb (n : Nat) (f : Fin n → Bool) : BitVec n := Id.run do
   let mut r : Nat := 0
 
   for i in range 0 (n/8) do
@@ -151,7 +151,7 @@ protected def generate_lsb (n:Nat) (f : Fin n → Bool) : BitVec n := Id.run do
     r := w <<< (8*(n/8)) ||| r
   ⟨r, sorry⟩
 
-protected def generate_msbb {n:Nat} (f : Fin n → Bool) : BitVec n := Id.run do
+protected def generate_msbb (n : Nat) (f : Fin n → Bool) : BitVec n := Id.run do
   let mut r : Nat := 0
 
   let m := n % 8
