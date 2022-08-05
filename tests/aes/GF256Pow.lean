6c2cdb8df8d80e042cfb2a909aab41f7864301cc
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-08-05 18:31:04 -0400

    feat: generalize GF2 to arbitrary exponent

--- tests/aes/GF256Pow.lean
+++ tests/aes/GF256Pow.lean
@@ -1,38 +1,38 @@
-import Crypto.GF2BV
-
 import Smt
 
-open GF256 GF2BVPoly
+import Crypto.GF2BV
+
+open GF2BVPoly GaloisField2k
 
 set_option smt.solver.kind "z3" in
-example (x : GF256) : pow 256 x = x := by
-  extract_def pow
+example (x : GF256) : GF256.pow2 8 x = x := by
+  extract_def GF256.pow2
+
   extract_def mul
-  extract_def polyMod
-  extract_def polyMul
+  specialize_def GaloisField2k.mul [GF256] blocking [@polyMod, @polyMul]
+  conv at GF256.pow2.def =>
+    intro k x
+    rw [ GaloisField2k.mul.GF256.specialization ]
   save
 
+  extract_def polyMod
   specialize_def GF2BVPoly.polyMod [16, 8]
   save
 
+  extract_def polyMul
   specialize_def GF2BVPoly.polyMul [8, 8]
   save
 
-  simp (config := {zeta := false}) only
-    [ GF2BVPoly.polyMul.«8».«8».specialization
-    , GF2BVPoly.polyMod.«16».«8».specialization
-    ]
-    at GF256.mul.def
-  save
+  conv at GaloisField2k.mul.GF256.def =>
+    intro a b
+    rw [ GF2BVPoly.polyMul.«8».«8».specialization,
+      GF2BVPoly.polyMod.«16».«8».specialization ]
 
   smt_show [
-    GF256,
-    GF256.irreducible,
-    GF256.mul,
-    GF256.pow.sq,
-    GF256.pow,
-    GF2BVPoly.polyMod.«16».«8»,
-    GF2BVPoly.polyMul.«8».«8»
-   ]
-
+    GF256.pow2,
+    GaloisField2k.mul.GF256,
+    GF2BVPoly.polyMul.«8».«8»,
+    GF2BVPoly.polyMod.«16».«8»
+  ] 
+    
   sorry
